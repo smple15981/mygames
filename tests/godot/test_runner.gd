9991844fe6f0e314a9293ea3a761b3be94245692
smple15981 <qivd36@163.com>
2026-08-04 23:02:35 +0800

    test: register player stats suite

--- tests/godot/test_runner.gd
+++ tests/godot/test_runner.gd
@@ -6,6 +6,7 @@ const SUITES := [
     preload("res://tests/godot/suites/test_crafting_service.gd"),
     preload("res://tests/godot/suites/test_inventory_ui.gd"),
     preload("res://tests/godot/suites/test_world_layout_config.gd"),
+    preload("res://tests/godot/suites/test_player_stats.gd"),
 ]
 
 
